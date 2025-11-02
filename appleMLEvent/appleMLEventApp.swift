001bc6b8777f61579e6a4bb0e0c8453f8262088b
Ayaan Jain <ayaanjainsg@gmail.com>
2025-11-02 14:23:42 +0800

    Initial Commit

---
+++ appleMLEvent/appleMLEventApp.swift
@@ -0,0 +1,32 @@
+//
+//  appleMLEventApp.swift
+//  appleMLEvent
+//
+//  Created by Ayaan Jain on 2/11/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct appleMLEventApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
